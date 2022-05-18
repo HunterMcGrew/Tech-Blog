dd2da2597b79bba548a8ecfde79fd256fe42a5b7
HunterLM <hunter109@gmail.com>
2022-05-17 21:52:42 -0500

    all-post, single-post HB, singup.js, home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,34 @@ const { Post, Comment, User } = require('../models/');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  
+  try {
+  let postData = await Post.findAll({
+    order: [
+      ["id", "ASC"]
+    ]
+  });
+  const posts = postData.map((post) => post.get({ plain: true }));
+  res.render("all-posts");
+
+} catch (err) {
+  if (err) throw err;
+}
+
 });
 
 // get single post
 router.get('/post/:id', async (req, res) => {
- 
+  try {
+    let postData = await Post.findByPk({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.render("single-post");
+
+  } catch (err) {
+    if (err) throw err;
+  }
 });
 
 router.get('/login', (req, res) => {
